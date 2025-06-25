e44bc876443cb141aa8427d292e8fa07c5ffcc5b
jydxh <wzxxds@gmail.com>
2025-06-25 13:37:52 -0400

    feat: add ScrollToTop component for automatic scrolling to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Router } from "react-router";
 import HomePage from "./pages/home.page";
 import About from "./pages/about.page";
 import Contact from "./pages/contact.page";
@@ -6,19 +6,23 @@ import PortfolioPage from "./pages/portfolio.page";
 import HomeLayout from "./pages/home_layout";
 import SubLayout from "./pages/SubLayout";
 import NotFoundPage from "./pages/notfound.page";
+import ScrollToTop from "./components/ScrollTop";
 
 export default function App() {
 	return (
-		<Routes>
-			<Route element={<HomeLayout />}>
-				<Route index element={<HomePage />} />
-				<Route element={<SubLayout />}>
-					<Route path="about" element={<About />} />
-					<Route path="contact" element={<Contact />} />
-					<Route path="portfolio" element={<PortfolioPage />} />
+		<>
+			<ScrollToTop />
+			<Routes>
+				<Route element={<HomeLayout />}>
+					<Route index element={<HomePage />} />
+					<Route element={<SubLayout />}>
+						<Route path="about" element={<About />} />
+						<Route path="contact" element={<Contact />} />
+						<Route path="portfolio" element={<PortfolioPage />} />
+					</Route>
+					<Route path="*" element={<NotFoundPage />} />
 				</Route>
-				<Route path="*" element={<NotFoundPage />} />
-			</Route>
-		</Routes>
+			</Routes>
+		</>
 	);
 }
